e76ca3b4e8ee7c254f3de4b5ad756d1b715d3c30
lucazwicky <schmucas@users.noreply.github.com>
2026-06-23 17:23:33 -0700

    add reader-utils, and pytest

---
+++ src/utils/reader_utils.py
@@ -0,0 +1,33 @@
+# Databricks notebook source
+from pyspark.sql import functions as F
+from pyspark.sql.window import Window
+from pyspark.sql import DataFrame
+
+# COMMAND ----------
+def dedupe(df: DataFrame, partition_cols: list, order_col: str, desc=True) -> DataFrame:
+    window_spec = (Window
+                   .partitionBy(*partition_cols)
+                   .orderBy(F.col(order_col).desc() if desc else F.col(order_col).asc()))
+    
+    return (df.withColumn("row_order", F.row_number().over(window_spec))
+                .filter(F.col("row_order") == 1)
+                .drop("row_order"))
+
+
+
+# COMMAND ----------
+def read_delta_table(table_conf: dict, start_date: str, end_date: str) -> DataFrame:
+    table = table_conf.get("table")
+    schema = table_conf.get("schema")
+    ts_col = table_conf.get("timestamp_col")
+    order_col = table_conf.get("order_col")
+
+    df = spark.read.table(f"{schema}.{table}").filter(F.col(ts_col).between(start_date, end_date))
+    
+    if "dedupe_desc" in SOURCE_TABLE_CONF:
+        return df.transform(dedupe, table_conf.get("dedupe_desc"), order_col)
+    
+    if "dedupe_asc" in SOURCE_TABLE_CONF:
+        return df.transform(dedupe, table_conf.get("dedupe_asc"), order_col, False)
+    
+    return df
--- tests/sample_taxis_test.py
+++
@@ -1,6 +0,0 @@
-from databricks_de_professional_practice import taxis
-
-
-def test_find_all_taxis():
-    results = taxis.find_all_taxis()
-    assert results.count() > 5
